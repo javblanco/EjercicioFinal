b1189fb8965f1fa4693a78279402f376efeb94ef
curso13 <curso13@noexiste.com>
2021-09-27 09:10:31 +0200

    Commit inicial

---
+++ src/test/java/es/cic/bootcamp/individual13final/Individual13finalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.bootcamp.individual13final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Individual13finalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
