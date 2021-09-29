8b3b9e6776671a19ee748fe99e5b9c542bb4204b
curso13 <curso13@noexiste.com>
2021-09-29 09:24:21 +0200

    Hablititado un test

--- src/main/angular/src/app/app.component.spec.ts
+++ src/main/angular/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  xit(`should have as title 'individual13-final'`, () => {
+  it(`should have as title 'Catálogo de obras de arte'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('individual13-final');
+    expect(app.title).toEqual('Catálogo de obras de arte');
   });
 
   xit('should render title', () => {
