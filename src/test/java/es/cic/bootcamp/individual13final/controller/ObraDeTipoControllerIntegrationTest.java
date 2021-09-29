cc4863fb4f8f8594f939951c8a595497978c3670
curso13 <curso13@noexiste.com>
2021-09-29 13:57:51 +0200

    Hechos los tests correspondientes a la tabla

---
+++ src/test/java/es/cic/bootcamp/individual13final/controller/ObraDeTipoControllerIntegrationTest.java
@@ -0,0 +1,184 @@
+package es.cic.bootcamp.individual13final.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.time.LocalDate;
+import java.time.Month;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.cic.bootcamp.individual13final.model.ObraDeTipo;
+import es.cic.bootcamp.individual13final.repository.ObraDeTipoRepository;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class ActividadDeProcesoControllerIntegrationTest { 
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	ObraDeTipoRepository obraDeTipoRepository;
+	
+	@Autowired
+	private EntityManager entityManager;	
+	
+	@Autowired
+    ObjectMapper mapper;	
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSave() throws Exception {
+		
+		ObraDeTipo obraDeTipo  = generarODT();
+
+		
+		 MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/obraTipo")
+		            .contentType(MediaType.APPLICATION_JSON)
+		            .accept(MediaType.APPLICATION_JSON)
+		            .content(this.mapper.writeValueAsString(obraDeTipo));
+		
+		  MvcResult result = this.mvc
+			.perform(mockRequest)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$",notNullValue()))
+			.andReturn();
+		
+		  String content = result.getResponse().getContentAsString();
+		  long idDelResultado = Long.parseLong(content);
+		  
+		  obraDeTipo.setId(idDelResultado);
+		  
+		  ObraDeTipo ObraDeTipoBBDD = entityManager.find(ObraDeTipo.class, idDelResultado);
+		  assertThat(ObraDeTipoBBDD)
+          	.usingRecursiveComparison()
+          	.isEqualTo(obraDeTipo);
+	}
+
+	
+	@Test
+	void testFindById() throws Exception {
+		ObraDeTipo odt = generarODT();
+		entityManager.persist(odt);
+		entityManager.flush();
+		
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/api/obraTipo/{id}", odt.getId())
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .accept(MediaType.APPLICATION_JSON);		
+		
+		this.mvc
+			.perform(mockRequest)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$",notNullValue()))
+			.andExpect(jsonPath("$.id", is(odt.getId().intValue())));
+	}
+
+	@Test
+	void testFindAll() throws Exception {
+		ObraDeTipo odt = generarODT();
+		entityManager.persist(odt);
+		
+		ObraDeTipo odt2 = generarODT();
+		entityManager.persist(odt2);
+		entityManager.flush();
+		
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/api/obraTipo")
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .accept(MediaType.APPLICATION_JSON);	
+		
+		
+		this.mvc
+		.perform(mockRequest)
+		.andDo(
+			print())
+		.andExpect(status().isOk())
+		.andExpect(
+			jsonPath("$",notNullValue()))
+		.andExpect(
+			jsonPath("$", hasSize(2)));
+	}
+
+	@Test
+	void testUpdate() throws Exception {
+		ObraDeTipo odt = generarODT();
+		entityManager.persist(odt);
+		entityManager.flush();
+		
+		ObraDeTipo odtModificado = generarODT();
+		odtModificado.setId(odt.getId());
+		odtModificado.setIdObra((long) 2);
+		
+		 MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/api/obraTipo")
+		            .contentType(MediaType.APPLICATION_JSON)
+		            .accept(MediaType.APPLICATION_JSON)
+		            .content(this.mapper.writeValueAsString(odtModificado));
+	
+		 
+		this.mvc
+		.perform(mockRequest)
+		.andDo(
+			print())
+		.andExpect(status().isOk());
+		
+		ObraDeTipo obraDeTipoBBDD = entityManager.find(ObraDeTipo.class, odtModificado.getId());
+
+		assertThat(obraDeTipoBBDD)
+      	.usingRecursiveComparison()
+      	.isEqualTo(odtModificado);
+	}
+
+	@Test
+	void testDeleteById() throws Exception {
+		ObraDeTipo odt = generarODT();
+		entityManager.persist(odt);
+		entityManager.flush();
+		
+		MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.delete("/api/obraTipo/{id}", odt.getId())
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .accept(MediaType.APPLICATION_JSON);
+	
+		this.mvc
+		.perform(mockRequest)
+		.andDo(
+			print())
+		.andExpect(status().isOk());
+
+		ObraDeTipo odtBBDD = entityManager.find(ObraDeTipo.class, odt.getId());
+	    assertNull(odtBBDD, "No borra el estilo");
+	}	
+
+	private ObraDeTipo generarODT() {
+		ObraDeTipo odt=new ObraDeTipo();
+		odt.setIdObra((long)1);
+		odt.setIdTipo((long)2);
+		return odt;
+	}
+}
---
+++ src/test/java/es/cic/bootcamp/individual13final/repository/ObraDeTipoRepositoryTest.java
@@ -0,0 +1,122 @@
+package es.cic.bootcamp.individual13final.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import es.cic.bootcamp.individual13final.model.ObraDeTipo;
+
+
+@DataJpaTest
+class ActividadDeProcesoRepositoryTest {
+
+	@Autowired
+	private ObraDeTipoRepository obraDeTipoRepository;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCrear() {
+		
+		ObraDeTipo obraDeTipo = generarODT();
+		
+		ObraDeTipo obraDeTipoGuardada = obraDeTipoRepository.save(obraDeTipo);
+		
+		entityManager.flush();
+		
+		ObraDeTipo obraDeTipoEnBBDD = entityManager.find(ObraDeTipo.class, obraDeTipoGuardada.getId());
+		
+		assertEquals(obraDeTipoGuardada, obraDeTipoEnBBDD, "No se ha podido guardar la obra del estilo");
+		
+
+	}
+
+	@Test
+	void testBuscar() {
+		
+		ObraDeTipo obraDeTipo = generarODT();
+		
+		ObraDeTipo odtALeer = entityManager.persist(obraDeTipo);
+		
+		ObraDeTipo odtBuscada = obraDeTipoRepository.findById(odtALeer.getId()).get();
+		
+		assertEquals(odtALeer, odtBuscada, "No se ha podido encontrar la obra del estilo");
+		
+	}
+
+	@Test
+	void testListar() {
+		
+		ObraDeTipo obraDeTipo = generarODT();
+		
+		ObraDeTipo odtALeer = entityManager.persist(obraDeTipo);
+		
+		ObraDeTipo obraDeTipo2 = generarODT();
+		
+		ObraDeTipo obraDeTipoALeer2 = entityManager.persist(obraDeTipo2);
+		
+		List<ObraDeTipo> obraDeTipos = List.of(odtALeer, obraDeTipoALeer2);
+		
+		List<ObraDeTipo> obraDeTiposLeidas = new ArrayList<>();
+		
+		obraDeTipoRepository.findAll()
+			.forEach(obraDeTiposLeidas::add);
+		
+		assertEquals(obraDeTipos.size(), obraDeTiposLeidas.size(), "No se han encontrado todas las obras del tipo");
+		
+	}
+	
+	@Test
+	void testModificar() {
+		
+		ObraDeTipo obraDeTipo = generarODT();
+		
+		ObraDeTipo obraDeTipoAModificar = entityManager.persist(obraDeTipo);
+		
+		ObraDeTipo obraDeTipoEnBaseDeDatos = obraDeTipoRepository.findById(obraDeTipoAModificar.getId()).get();
+		
+		obraDeTipoEnBaseDeDatos.setIdObra((long) 2);		
+		
+		entityManager.flush();
+		
+		ObraDeTipo obraDeTipoActualizada = obraDeTipoRepository.findById(obraDeTipoAModificar.getId()).get();
+		
+		assertEquals((long) 2,obraDeTipoActualizada.getIdObra());				
+		
+		
+	}
+
+	@Test
+	void testBorrar() {
+		
+		ObraDeTipo obraDeTipo = generarODT();
+		
+		ObraDeTipo obraDeTipoABorrar = entityManager.persist(obraDeTipo);
+		
+		entityManager.flush();
+		
+		obraDeTipoRepository.deleteById(obraDeTipoABorrar.getId());
+		
+		ObraDeTipo actividadBorrada = entityManager.find(ObraDeTipo.class, obraDeTipoABorrar.getId());
+		
+		assertNull(actividadBorrada, "No se ha podido borrar la actividad de proceso");
+		
+	}
+	
+	private ObraDeTipo generarODT() {
+		ObraDeTipo odt=new ObraDeTipo();
+		odt.setIdObra((long)1);
+		odt.setIdTipo((long)2);
+		return odt;
+	}
+}
